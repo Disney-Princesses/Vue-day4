3b946e5dba5855a1871ef22b49562a82b5b5b76a
limengting <1443848358@qq.com>
2022-07-01 20:06:37 +0800

    computed属性部分练习

---
+++ src/03.vue计算属性--computed.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    购物车商品总价：
+    <!-- 商品是否大于500 符合条件可使用400的优惠券 否则不可使用 -->
+    <!-- {{ total * price >= 500 ? total * price - coupon : total * price }} -->
+    <!-- 在模板中放入太多的逻辑，这样会导致 会让模板过重， 并且代码 很难以维护 -->
+    <!-- 解决方法  vue计算属性-computed -->
+    {{totalPrice}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      total: 5, // 商品数量
+      price: 98, // 单价
+      coupon: 400 // 优惠券
+    };
+  },
+  computed: {
+    totalPrice() {
+      return this.total * this.price >= 500
+        ? this.total * this.price - this.coupon
+        : this.total * this.price;
+    }
+  }
+};
+</script>
---
+++ src/05.vue计算属性完整写法练习.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div>
+      <span>姓:</span>
+      <input type="text" v-model="firstName" />
+      <span>名:</span>
+      <input type="text" v-model="lastName" />
+      <span>全名:</span>
+      <input type="text" v-model="fullName" />
+    </div>
+  </div>
+</template>
+
+<script>
+// 问题: 给计算属性赋值 - 需要setter
+// 解决:
+/*
+    完整语法:
+    computed: {
+        "计算属性名" (){},
+        "计算属性名": {
+            set(值){
+
+            },
+            get(){
+                return 值
+            }
+        }
+    }
+*/
+export default {
+  data() {
+    return {
+      firstName:'',
+      lastName:'',
+    };
+  },
+  computed: {
+    fullName: {
+      // 给fullName赋值触发set方法
+      set(val) {
+        const arr = val.split('^')
+        this.firstName=arr[0]
+        this.lastName=arr[1]
+      },
+      // 使用fullName的值触发get方法
+      get() {
+        return this.firstName+'^'+this.lastName;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
