eb62bfd8fda04a31aef30e8a5001b0b35d90322b
tianbingyao22 <2242378609@qq.com>
2022-07-01 19:57:19 +0800

    完成了品牌管理案例中的vue过滤器部分

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css" // 默认找文件夹下的index文件(但是这个不是所以需要写路径)
+import moment from "moment"
 
 Vue.config.productionTip = false
+Vue.filter("formate",(val)=>{
+  return moment(val).format("YYYY-MM-DD  HH:mm:ss")
+})
 
 new Vue({
   render: h => h(App),
