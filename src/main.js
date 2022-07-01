3ca0182276bf826c41d5d1e2a6c29a6f6d4d8cd7
fengbujue <1125238900@qq.com>
2022-07-01 20:32:48 +0800

    完成了过滤时间

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import moment from 'moment'
 
 Vue.config.productionTip = false
+
+Vue.filter('setDate', (val, str) => {
+  return moment(val).format(`YYYY${str}MM${str}DD`)
+})
 
 new Vue({
   render: h => h(App),
