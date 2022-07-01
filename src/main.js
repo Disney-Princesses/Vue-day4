61c3a7139a25678f35b016010f0405ae7c4c67be
limengting <1443848358@qq.com>
2022-07-01 21:12:36 +0800

    过滤器作业

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import moment from 'moment'
 
 Vue.config.productionTip = false
+
+Vue.filter('formateDate',function(val,timearg) {
+  return moment(val).format(timearg)
+})
 
 new Vue({
   render: h => h(App),
