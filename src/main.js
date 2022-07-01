2a5ab5feb614261185c6b8c6a2351634fc3a8abb
GuanSzzz <847388139@qq.com>
2022-07-01 22:40:50 +0800

    完成了品牌管理案例

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+// 全局过滤器
 import moment from "moment";
 Vue.filter('setDate', (val, n) => {
   return moment(val).format('YYYY' + n + 'MM' + n + 'DD')
 })
+
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css"
 
 new Vue({
   render: h => h(App),
---
+++ src/时间过滤器.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>时间：{{ date | setDate("-") }}</p>
+    <p>时间：{{ date | setDate("/") }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      date: 1636450540055,
+    };
+  },
+};
+</script>
+
+<style></style>
